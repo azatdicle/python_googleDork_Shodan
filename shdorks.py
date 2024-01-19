3bdbfdfa4ae7b834294c745ae97e4426505fc406
AZAT DİCLE <75863129+azatdicle@users.noreply.github.com>
2024-01-20 01:28:29 +0300

    Update shdorks.py

--- shdorks.py
+++ shdorks.py
@@ -1,32 +1,14 @@
 import shodan
-import requests
-import argparse
-from functools import partial
-from multiprocessing import Pool
-from bs4 import BeautifulSoup as bsoup
+from googlesearch import search
 
-def google_dork_search(query):
-    search_url = f"https://www.google.com/search?q={query}"
-    response = requests.get(search_url)
-    return response.text
+def google_search(query):
+    try:
+        search_results = search(query)
+        return search_results
+    except Exception as e:
+        print(f"Hata: {e}")
+        return []
 
-def extract_links(text):
-    # Düzenli ifade ile linkleri ayıkla
-    pattern = re.compile(r'href=["\'](https?://\S+)', re.IGNORECASE)
-    links = pattern.findall(text)
-    return links
-
-def google_dorks():
-    query = input("Google Dork: ")
-    result = google_dork_search(query)
-    links = extract_links(result)
-    
-    # Elde edilen linkleri yazdır
-    print("Found Links:")
-    for link in links:
-        print(link)
-        
-        
 def shodan_search(api_key):
     api = shodan.Shodan(api_key)
     query = input("Shodan Search: ")
@@ -40,18 +22,25 @@ def shodan_search(api_key):
     except shodan.APIError as e:
         print(f'Hata: {e}')
 
-print("""
+if __name__ == "__main__":
+    print("""
       /| ________________
 O|===|* >________________>
-      \|                        
+      \|
 Google Dorks and Shodan 
       @azatdicle                               
                                                                       
       """)
 
-use = input("1. Google Dork\n2. Shodan\n:")
-if use == "1":
-    google_dorks()
-elif use == "2":
-    api_key = input("YOUR_SHODAN_API_KEY: ")
-    shodan_search(api_key)
+    use = input("1. Google Dork\n2. Shodan\n:")
+    if use == "1":
+        search_query = input("Dork:")
+        search_results = google_search(search_query)
+
+        print("Google Search Results:")
+        for result in search_results:
+            print(result)
+            
+    elif use == "2":
+        api_key = input("YOUR_SHODAN_API_KEY: ")
+        shodan_search(api_key)
